a87d8cec73ef9f38b3ac33e404ed89e7cd30333f
lokimve7 <lokimve7@gmail.com>
2024-01-16 12:13:21 +0900

    .

--- Source/CPPTps/Private/AnimPlayer.cpp
+++ Source/CPPTps/Private/AnimPlayer.cpp
@@ -20,11 +20,21 @@ void UAnimPlayer::NativeUpdateAnimation(float DeltaSeconds)
 		dirF = FVector::DotProduct(forward, moveDir);
 		//dirF = forward.Dot(moveDir);
 
+		//// 각도로 표현해보자 ( 각도 = arcos (두 벡터의 내적)) 
+		//// 단 각도는 radian 로 표현된다.
+		//float dot = FVector::DotProduct(forward, moveDir.GetSafeNormal());
+		//float radianAngle = FMath::Acos(dot);
+		//float degreeAngle = radianAngle * (180 / UE_PI);
+		////FMath::DegreesToRadians(90);
+		////FMath::RadiansToDegrees(radianAngle)
+		//UE_LOG(LogTemp, Warning, TEXT("angle : %f"), degreeAngle);
+		
+
 		// 나의 오른쪽 방향
 		FVector right = pawn->GetActorRightVector();
 		// right 와 moveDir 의 내적 = cos(두 벡터가 이루는 각) 
 		// 위 값이 0보다 크면 오른쪽 방향, 0보다 작으면 왼쪽 방향
 		dirR = FVector::DotProduct(right, moveDir);
-		UE_LOG(LogTemp, Warning, TEXT("dot : %f"), dirR);
+		//UE_LOG(LogTemp, Warning, TEXT("dot : %f"), dirR);
 	}
 }
